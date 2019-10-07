cc402e054edd8e5401fa56ad373e8b072d02f002
Jakub <j_bednarz28.03.92@wp.pl>
2019-10-07 14:54:19 +0200

    Updates

---
+++ precache-manifest.9a0a8ef738cf2b6850e8715b2bbdbbec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77ed52a42a1073da5429976d18503fb",
+    "url": "/Cinema-App/index.html"
+  },
+  {
+    "revision": "f092ff817d0aea38e3ef",
+    "url": "/Cinema-App/static/css/main.b83fcd56.chunk.css"
+  },
+  {
+    "revision": "6c7f325114f203fbeb0f",
+    "url": "/Cinema-App/static/js/2.cfa060a3.chunk.js"
+  },
+  {
+    "revision": "f092ff817d0aea38e3ef",
+    "url": "/Cinema-App/static/js/main.d8839b3e.chunk.js"
+  },
+  {
+    "revision": "b990d57d472183c1731b",
+    "url": "/Cinema-App/static/js/runtime-main.3e352e7c.js"
+  }
+]);
